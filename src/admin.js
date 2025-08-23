3ea4f610df9c3daed15c1de6fdc703d6d17e5dd0
thisYogesh <yog99mail@gmail.com>
2025-08-24 01:28:38 +0530

    implementing extractFields logic

--- src/admin.js
+++ src/admin.js
@@ -1,9 +1,27 @@
+import Handlebars from "handlebars";
 const getEval = path => new Function("obj", `return obj.${path}`);
 
 const getFields = value =>
   [...Object.entries(value)]
     .filter(([_, value]) => value.type)
     .map(([title, value]) => ({ title, value }));
+
+Handlebars.registerHelper({
+  dynamic_field: function(fieldObj) {
+    const { value } = fieldObj.field;
+    const { type } = value;
+    return type;
+  }
+});
+
+Handlebars.registerPartial({
+  image: ``,
+  button: `
+  <div class="main border p-2">
+    <label>{{title}}</label>
+    <input type="text" value="{{value.label}}"/>
+  </div>`
+});
 
 const Customiser = {
   init(e) {
@@ -51,70 +69,94 @@ const Customiser = {
   showConfig(path) {
     const { storeConfig } = this;
     const data = getEval(path)(storeConfig);
+    const fields = [];
 
-    const fields = [...Object.entries(data)].map(([title, value]) =>
-      this.makeField({ title, value })
-    );
-
-    console.log(fields);
+    if (data.type) {
+      fields.push(this.makeField({ title: path, value: data }));
+    } else {
+      fields.push(
+        ...getFields(data).map(({ title, value }) =>
+          this.makeField({ title, value })
+        )
+      );
+    }
 
     this.$fieldViewer.innerHTML = fields.join("");
   },
 
-  makeField({ title, value }) {
+  extractFields(field) {
+    const { title, value } = field;
+    const fields = {
+      field,
+      fields: getFields(value).map(field => this.extractFields(field))
+    };
+    return fields;
+  },
+
+  makeField(field) {
+    const { title, value } = field;
     const isString = typeof value === "string";
 
-    if (value.type === "button") {
-      return `<div class="border p-2">
-        <label>${title}</label>
-        <input type="text" value="${value.label}"/>
+    const template = Handlebars.compile(`
+      <div class="root border p-2">
+        {{#if this.field}}
+          {{> (dynamic_field this) }}
+        {{/if}}
+      </div>`);
 
-        <div>
-          ${getFields(value)
-            .map(field => this.makeField(field))
-            .join("")}
-        </div>
-      </div>`;
-    } else if (value.type === "link-button") {
-      return `<div class="border p-2">
-        <label>${title}</label>
-        <input type="text" value="${value.label}"/>
+    console.log(this.extractFields(field));
 
-        <div>
-          ${getFields(value)
-            .map(field => this.makeField(field))
-            .join("")}
-        </div>
-      </div>`;
-    } else if (value.type === "color") {
-      return `<div class="border p-2">
-        <label>${title}</label>
-        <input type="color" value="${value.value}"/>
+    // if (value.type === "button") {
+    //   return `<div class="border p-2">
+    //     <label>${title}</label>
+    //     <input type="text" value="${value.label}"/>
 
-        <div>
-          ${getFields(value)
-            .map(field => this.makeField(field))
-            .join("")}
-        </div>
-      </div>`;
-    } else if (value.type === "size") {
-      return `<div class="border p-2">
-        <label>${title}</label>
-        <div class="flex gap-1">
-          <input type="text" value="${value.height}"/>
-          <input type="text" value="${value.width}"/>
-        </div>
+    //     <div>
+    //       ${getFields(value)
+    //         .map(field => this.makeField(field))
+    //         .join("")}
+    //     </div>
+    //   </div>`;
+    // } else if (value.type === "link-button") {
+    //   return `<div class="border p-2">
+    //     <label>${title}</label>
+    //     <input type="text" value="${value.label}"/>
 
-        <div>
-          ${getFields(value)
-            .map(field => this.makeField(field))
-            .join("")}
-        </div>
-      </div>`;
-    } else if (isString)
-      return `<div class="border p-2"><label>${title}</label> <input type="text" value="${value}"/></div>`;
+    //     <div>
+    //       ${getFields(value)
+    //         .map(field => this.makeField(field))
+    //         .join("")}
+    //     </div>
+    //   </div>`;
+    // } else if (value.type === "color") {
+    //   return `<div class="border p-2">
+    //     <label>${title}</label>
+    //     <input type="color" value="${value.value}"/>
 
-    return "";
+    //     <div>
+    //       ${getFields(value)
+    //         .map(field => this.makeField(field))
+    //         .join("")}
+    //     </div>
+    //   </div>`;
+    // } else if (value.type === "size") {
+    //   return `<div class="border p-2">
+    //     <label>${title}</label>
+    //     <div class="flex gap-1">
+    //       <input type="text" value="${value.height}"/>
+    //       <input type="text" value="${value.width}"/>
+    //     </div>
+
+    //     <div>
+    //       ${getFields(value)
+    //         .map(field => this.makeField(field))
+    //         .join("")}
+    //     </div>
+    //   </div>`;
+    // } else if (isString)
+    //   return `<div class="border p-2"><label>${title}</label> <input type="text" value="${value}"/></div>`;
+
+    return template({ field, fields: getFields(value) });
   }
 };
 
