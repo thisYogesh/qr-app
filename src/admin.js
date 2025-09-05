cc7a808744fbd34c225c388554c8a848a4bd1d74
thisYogesh <yog99mail@gmail.com>
2025-09-06 00:01:28 +0530

    added tailwind

--- src/admin.js
+++ src/admin.js
@@ -34,7 +34,20 @@ Handlebars.registerHelper({
           {{/if}}
         </div>
 
-        <button class="button">Upload Image</button>
+        <div class="flex gap-1">
+          <button class="button self-start basis-0">
+            Upload Image
+            <span class="sep w-[2px]"></span>
+
+            <svg class="w-4 h-4" viewBox="0 0 24 24" fill="none">
+              <path
+      d="M19 9L14 14.1599C13.7429 14.4323 13.4329 14.6493 13.089 14.7976C12.7451 14.9459 12.3745 15.0225 12 15.0225C11.6255 15.0225 11.2549 14.9459 10.9109 14.7976C10.567 14.6493 10.2571 14.4323 10 14.1599L5 9"
+      stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+            </svg>
+          </button>
+          <button class="button self-start basis-0">From URL</button>
+          <button class="button self-start basis-0">Paste SVG</button>
+        </div>
       </div>
     `)(obj);
   },
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-// import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 const injectBuildHash = () => ({
   name: "build-hash",
   generateBundle(_, bundle) {
     try {
-      const file = Object.values(bundle).find((file) => file.name === "index");
+      const file = Object.values(bundle).find(file => file.name === "index");
       const contentKey = file.type === "chunk" ? "code" : "source";
 
       file[contentKey] = file[contentKey].replace(
@@ -15,18 +15,18 @@ const injectBuildHash = () => ({
     } catch {
       console.log("Error in [build-hash] plugin!");
     }
-  },
+  }
 });
 
 export default defineConfig({
-  plugins: [injectBuildHash()],
+  plugins: [tailwindcss(), injectBuildHash()],
   assetsInclude: ["**/*.lottie"],
   build: {
     rollupOptions: {
       input: {
         main: "index.html",
-        admin: "admin.html",
-      },
-    },
-  },
+        admin: "admin.html"
+      }
+    }
+  }
 });
